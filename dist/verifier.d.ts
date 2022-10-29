cbc4a93fdf7d7439ad485c844bd98b1a9726de7d
Aditya Chakraborty <adityachakra16@gmail.com>
2022-10-28 19:34:33 -0500

    Update verifier for node

---
+++ dist/verifier.d.ts
@@ -0,0 +1,12 @@
+import { PassportReader } from "@gitcoinco/passport-sdk-reader";
+import { DIDKitLib, Passport, Stamp, VerifiableCredential } from "@gitcoinco/passport-sdk-types";
+export declare class PassportVerifier {
+    _DIDKit: DIDKitLib;
+    _reader: PassportReader;
+    _network: string;
+    constructor(url?: string, network?: string);
+    init(): Promise<void>;
+    verifyPassport(address: string, passport?: Passport, additionalStampChecks?: (stamp: Stamp) => boolean): Promise<Passport | false>;
+    verifyStamp(address: string, stamp: Stamp, additionalStampChecks?: (stamp: Stamp) => boolean): Promise<Stamp | false>;
+    verifyCredential(credential: VerifiableCredential): Promise<boolean>;
+}
